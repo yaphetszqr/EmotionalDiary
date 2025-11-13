bf40e08820cea1dc17cbe092e91d2028fc30cd5b
yaphetszqr <yaphetszqr1@gmail.com>
2025-11-13 19:51:12 +0800

    first project

---
+++ app/src/main/java/com/example/myapplication/JSBridge.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class JSBridge {
+    private Context context;
+
+    public JSBridge(Context context) {
+        this.context = context;
+    }
+
+    // 暴露给 JavaScript 调用的方法
+    @JavascriptInterface
+    public void showToast(String message) {
+        Toast.makeText(context, "来自JS的信息: " + message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        // 保证在 WebView 内部打开网页，而不是跳浏览器
+        webView.setWebViewClient(new WebViewClient());
+
+        // 解决部分设备黑屏
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+
+        // 注册 JS 接口
+        webView.addJavascriptInterface(new JSBridge(this), "AndroidBridge");
+
+        // 加载远程网页
+         webView.loadUrl("https://www.baidu.com");
+
+        // 或加载本地网页（放在 assets 目录）
+//        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    // 支持按返回键返回上一页
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
